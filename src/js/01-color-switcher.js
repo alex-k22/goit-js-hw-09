acb77f19c195bb92d276c9ecd3e9c68b3a245d57
Alexey Kovalov <kovalov.alexey@gmail.com>
2023-04-01 14:52:56 +0000

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
 
+const startButtonEl = document.querySelector('[data-start]');
+const stopButtonEl = document.querySelector('[data-stop]');
+let timerId;
+
+handleOnStartClick = () =>{
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    startButtonEl.disabled = true;
+}
+
+handlerOnStopClick = () => {
+    startButtonEl.disabled = false;
+    clearInterval(timerId);
+}
+
+startButtonEl.addEventListener('click', handleOnStartClick);
+
+stopButtonEl.addEventListener('click', handlerOnStopClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
